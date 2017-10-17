678be60f16fa84e6c05090c9828984b49e6e4140
Anton Babakhin <anton-098@yandex.ru>
2017-10-17 23:00:43 +0300

    version 2.0

--- s.mjs
+++ s.mjs
@@ -1,8 +1,14 @@
 import express from 'express';
-const PORT=4321;
+const PORT=4321,
+      CORS = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,POST,DELETE',
+        'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
+      };
 express()
   .use(express.static('public'))
   .get('/', r=>r.res.sendFile('public/index.html'))
+  .get('/author', r=>r.res.set(CORS).send('Бабахин Антон'))
   .use(r=>r.res.status(404).end('Still not here, sorry!'))
   .use((e,r,res,n)=>res.status(500).end(`Error: ${e}`))
   .listen(process.env.PORT || PORT, ()=>console.log(process.pid))
